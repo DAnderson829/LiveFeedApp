e83704f3c4fb5075036a631ead15232aeb2d697a
DAnderson829 <dylandoku@gmail.com>
2025-09-26 11:45:11 -0400

    Initial commit

---
+++ LiveFeed/src/main/java/com/dylan/LiveFeed/LiveFeedApplication.java
@@ -0,0 +1,13 @@
+package com.dylan.LiveFeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveFeedApplication.class, args);
+	}
+
+}
---
+++ LiveFeed/src/test/java/com/dylan/LiveFeed/LiveFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylan.LiveFeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
